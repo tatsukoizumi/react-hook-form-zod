f2c56b86d3a6e1d3dbd467d7acd4b99d17ddf874
tatsukoizumi <koizumitattsuu@gmail.com>
2022-09-17 21:47:51 +0900

    Create simple form with rhf

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,15 +1,30 @@
 import { useId } from "react"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
+const scheme = z.object({
+  familyName: z.string(),
+  givenName: z.string(),
+})
+
+type FormValue = z.infer<typeof scheme>
 
 export const Form = () => {
+  const { register, handleSubmit } = useForm<FormValue>()
+
+  const onSubmit = (value: FormValue) => {
+    window.alert(value)
+  }
+
   const id1 = useId()
   const id2 = useId()
 
   return (
-    <form>
-      <label htmlFor={id1}>item1</label>
-      <input id={id1} type="text" />
-      <label htmlFor={id2}>item2</label>
-      <input id={id2} type="text" />
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <label htmlFor={id1}>familyName</label>
+      <input id={id1} {...register("familyName")} type="text" />
+      <label htmlFor={id2}>givenName</label>
+      <input id={id2} {...register("givenName")} type="text" />
       <button type="submit">Submit</button>
     </form>
   )
