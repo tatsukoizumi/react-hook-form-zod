dddf32dd6037fa353b5a33d19ee18b52d8d0770b
tatsukoizumi <koizumitattsuu@gmail.com>
2022-09-17 22:01:54 +0900

    Implement simple form with zod resolver

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,29 +1,33 @@
 import { useId } from "react"
-import { useForm } from "react-hook-form"
+import { SubmitHandler, useForm } from "react-hook-form"
 import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import "./form.css"
 
 const scheme = z.object({
-  familyName: z.string(),
-  givenName: z.string(),
+  familyName: z.string().min(1),
+  givenName: z.string().min(1).optional(),
 })
 
 type FormValue = z.infer<typeof scheme>
 
 export const Form = () => {
-  const { register, handleSubmit } = useForm<FormValue>()
+  const { register, handleSubmit } = useForm<FormValue>({
+    resolver: zodResolver(scheme),
+  })
 
-  const onSubmit = (value: FormValue) => {
-    window.alert(value)
+  const onSubmit: SubmitHandler<FormValue> = value => {
+    console.log({ value })
   }
 
   const id1 = useId()
   const id2 = useId()
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
+    <form onSubmit={handleSubmit(onSubmit)} className="form-container">
       <label htmlFor={id1}>familyName</label>
       <input id={id1} {...register("familyName")} type="text" />
-      <label htmlFor={id2}>givenName</label>
+      <label htmlFor={id2}>givenName(optional)</label>
       <input id={id2} {...register("givenName")} type="text" />
       <button type="submit">Submit</button>
     </form>
